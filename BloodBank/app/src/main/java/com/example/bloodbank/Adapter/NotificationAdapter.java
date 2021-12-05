1b5e11d9e32e109375be0d78d362f30df64e2c8a
Mira Mansour <81388088+m-mansour1@users.noreply.github.com>
2021-12-06 01:54:14 +0200

    test change in notificationAdapter

--- BloodBank/app/src/main/java/com/example/bloodbank/Adapter/NotificationAdapter.java
+++ BloodBank/app/src/main/java/com/example/bloodbank/Adapter/NotificationAdapter.java
@@ -23,6 +23,7 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
+        
     }
 
     @Override
