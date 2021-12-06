6096f6fca167c31fbf24ab583321784b5d29104e
Mira Mansour <81388088+m-mansour1@users.noreply.github.com>
2021-12-06 02:00:13 +0200

    some changes

--- BloodBank/app/src/main/java/com/example/bloodbank/Adapter/NotificationAdapter.java
+++ BloodBank/app/src/main/java/com/example/bloodbank/Adapter/NotificationAdapter.java
@@ -1,5 +1,6 @@
 package com.example.bloodbank.Adapter;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -7,12 +8,23 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.bloodbank.Model.Notification;
 import com.example.bloodbank.R;
+
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.ViewHolder>{
 
+
+    private Context context;
+    private List<Notification> notificationList;
+
+    public NotificationAdapter(Context context, List<Notification> notificationList) {
+        this.context = context;
+        this.notificationList = notificationList;
+    }
 
     @NonNull
     @Override
@@ -23,7 +35,7 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
-        
+
     }
 
     @Override
