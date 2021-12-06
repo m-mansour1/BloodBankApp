7c1869efd704c9d303512ba6663b80f237d0947a
Mira Mansour <81388088+m-mansour1@users.noreply.github.com>
2021-12-06 19:10:45 +0200

    Update Util.java

--- BloodBank/app/src/main/java/com/example/bloodbank/Email/Util.java
+++ BloodBank/app/src/main/java/com/example/bloodbank/Email/Util.java
@@ -3,7 +3,7 @@ package com.example.bloodbank.Email;
 public class Util {
 
     public static final String EMAIL = "miramansour491@gmail.com";
-    public static final String PASSWORD = "Mireille2001";
+    public static final String PASSWORD = "";// password should be included to send the mail 
 
 
 }
